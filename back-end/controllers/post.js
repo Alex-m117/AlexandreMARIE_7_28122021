6d02e08513a7c723bfc1d0d22145cc7b7a985a5f
Alexandre MARIE <Alex.m117@outlook.fr>
2022-01-10 23:06:18 +0100

    mise à jour back

--- back-end/controllers/post.js
+++ back-end/controllers/post.js
@@ -7,30 +7,31 @@ const result = dotenv.config();
 const data = bdd.database();
 
 exports.createPost = (req, res, next) => {
+  console.log(req.body.userId)
+  console.log(req.file)
+    const userId = token.tokenUserId(req);
+    console.log(userId)
 
-  const userId = token.tokenUserId(req);
-  console.log('userId')
-  console.log(userId)
-  let { body, file } = req;
-    if (!file) delete req.body.image;
-      body = {...body};
-
-  const db = `INSERT INTO Posts ()`;
-  data.query(db, body,(err, result) => {
-
-      console.log("result")
-      console.log(result)
-    if (err)  {
-      console.log(err)
-      return res.status(404).json ({ message: "Ajout des informations impossible !" }); 
+    const texte = req.body.message;
+    const image = (req.file) ? `${req.protocol}://${req.get('host')}/images/${req.file.filename}`: "";   
+    const post = {
+        message: texte,
+        image: image,
+        //date: new Date().toLocaleDateString("af_AZ", { timeZone : "Paris/Europe" }),
+        userId: userId,
     }
-    if (result) {
+    console.log(req.file)
+    console.log(req.body)
+   console.log(post)
+    const dbPost = `INSERT INTO Posts (message, image, userId) VALUES (?)`;
+    data.query(dbPost, post,(err, result) => {
+      if (err)  {
+           console.log(err)
+           console.log(result)
+        return res.status(404).json ({ message: "Ajout des informations impossible !" }); 
+      }
+      if (result) {
         res.status(200).json ({ result }); 
-    };
-  });
-
- 
-
-
-
+      };
+    });
   };

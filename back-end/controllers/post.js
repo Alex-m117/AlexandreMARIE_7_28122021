8135a75ca89fb51839eeb861a1ca1c73c0754bb7
Alexandre MARIE <Alex.m117@outlook.fr>
2022-01-11 03:16:49 +0100

    tentative sql sur post a verif

--- back-end/controllers/post.js
+++ back-end/controllers/post.js
@@ -7,30 +7,29 @@ const result = dotenv.config();
 const data = bdd.database();
 
 exports.createPost = (req, res, next) => {
-  console.log(req.body.userId)
+  console.log('--->-')
+  console.log(req)
   console.log(req.file)
     const userId = token.tokenUserId(req);
     console.log(userId)
 
-    const texte = req.body.message;
-    const image = (req.file) ? `${req.protocol}://${req.get('host')}/images/${req.file.filename}`: "";   
     const post = {
-        message: texte,
-        image: image,
-        //date: new Date().toLocaleDateString("af_AZ", { timeZone : "Paris/Europe" }),
+        message: req.body.message,
+        image: (req.file) ? `${req.protocol}://${req.get('host')}/images/${req.file.filename}`: "", 
+        date: new Date().toLocaleDateString(),
         userId: userId,
     }
     console.log(req.file)
     console.log(req.body)
    console.log(post)
-    const dbPost = `INSERT INTO Posts (message, image, userId) VALUES (?)`;
+    const dbPost = "INSERT INTO Posts (message, image, date, userId) VALUES ('?')";
     data.query(dbPost, post,(err, result) => {
       if (err)  {
            console.log(err)
            console.log(result)
         return res.status(404).json ({ message: "Ajout des informations impossible !" }); 
       }
-      if (result) {
+      else  {
         res.status(200).json ({ result }); 
       };
     });
